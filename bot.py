b13be67063ef44734a26b4975fa66771e372204d
Evandro-cyber-lab <evandroferreira070796@gmail.com>
2025-05-02 18:32:41 -0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,7 @@
+import time
+
+def start_bot():
+    print("Iniciando IA Investidora...")
+    while True:
+        print("Analisando o mercado e tomando decisões...")
+        time.sleep(60)  # Espera 1 minuto antes da próxima análise
